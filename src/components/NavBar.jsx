d57cdbaaff6640dfbf014704a51d16cbfe069fa7
Jonathan Arias <ariasjona1998@gmail.com>
2025-07-28 20:55:27 -0300

    correccion en estilos

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,20 +1,27 @@
 import CartWidget from "./CartWidget";
 
-
 const NavBar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-4">
-      <a className="navbar-brand" href="#">Mi Tienda</a>
+      <a className="navbar-brand" href="#">
+        Mi Tienda
+      </a>
       <div className="collapse navbar-collapse">
         <ul className="navbar-nav me-auto">
           <li className="nav-item">
-            <a className="nav-link" href="#">Inicio</a>
+            <a className="nav-link" href="#">
+              Inicio
+            </a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">Productos</a>
+            <a className="nav-link" href="#">
+              Productos
+            </a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">Contacto</a>
+            <a className="nav-link" href="#">
+              Contacto
+            </a>
           </li>
         </ul>
         <CartWidget />
